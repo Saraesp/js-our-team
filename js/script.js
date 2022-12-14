a682300b1485d008d6cfc197f0b4c1a5d19820ff
Saraesp <saraesposito145@gmail.com>
2022-12-14 20:57:30 +0100

    impostazione esercizio js MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,51 @@
+//ESERCIZIO
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+// MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe.
+
+// Una volta che avete terminato una milestone, fare un push e nel messaggio indicare che avete terminato la milesione numero X.
+
+// BONUS (da fare una volta terminate le singole milestone)
+// BONUS 1: Trasformare la stringa foto in una immagine effettiva
+// BONUS 2: Organizzare i singoli membri in card/schede
+
+// MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+const teams = [
+    { 
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'Wayne-barnett-founder-ceo.jpg',
+    },
+
+    { 
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+
+    { 
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+
+    { 
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    { 
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+
+    { 
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
